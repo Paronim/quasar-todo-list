a7571a09be7b9363d5d7115b935ec7906738c36a
Paronim <Zreba.v@gmial.com>
2023-03-09 11:35:51 +0300

    Стилизация задач и функционал выполнения задачи

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -2,17 +2,34 @@
   <q-page class="bg-orange-3 column q-pa-lg">
     <h5>Todo</h5>
 
-    <q-list>
-      <q-item tag="label"
+    <q-list 
+    separator
+    bordered
+    >
+      <q-item 
+      @click="task.done = !task.done"
+      clickable
+      :class="{'done bg-green-4': task.done}"
       v-for="task in tasks"
       :key="task.title"
       v-ripple
       >
         <q-item-section avatar>
-          <q-checkbox v-model="color" val="orange" color="primary" />
+          <q-checkbox
+          class="no-pointer-events"
+          v-model="task.done" 
+          val="orange" 
+          color="primary" 
+          />
         </q-item-section>
         <q-item-section>
-          <q-item-label>Orange</q-item-label>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section
+        v-if="task.done"
+        side
+        >
+          x
         </q-item-section>
       </q-item>
 
@@ -46,3 +63,12 @@ export default{
 }
 
 </script>
+
+<style lang="scss">
+.done{
+  .q-item__label{
+    text-decoration: line-through;
+    color: rgb(53, 53, 53)
+  }
+}
+</style>
