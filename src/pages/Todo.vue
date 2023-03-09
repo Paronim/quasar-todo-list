2a5e632358077be97ff6a2106da35f0ac03cd582
Paronim <Zreba.v@gmial.com>
2023-03-09 10:52:56 +0300

    Добавлены Checkbox и список задач

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,9 +1,48 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="bg-orange-3 column q-pa-lg">
     <h5>Todo</h5>
+
+    <q-list>
+      <q-item tag="label"
+      v-for="task in tasks"
+      :key="task.title"
+      v-ripple
+      >
+        <q-item-section avatar>
+          <q-checkbox v-model="color" val="orange" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Orange</q-item-label>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
+
+export default{
+  data() {
+    return {
+      tasks: [
+        {
+        title: "hi",
+        done: false
+        },
+        {
+        title: "h1",
+        done: false
+        },
+        {
+        title: "h2",
+        done: false
+        },
+      ]
+      
+    }
+  }
+
+}
 
 </script>
